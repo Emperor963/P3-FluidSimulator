652c83d6d2d8d5d714856aeb2d3f878c7ca31cc4
Emperor963 <bhattachar37@wisc.edu>
2024-05-01 07:18:26 -0500

    Changes made to Simulator class

--- fluids.js
+++ fluids.js
@@ -42,6 +42,68 @@ class Particle {
 
 }
 
+
+/**
+ * Object oriented design of the simulator. This class should contain all relevant code pertaining to
+ * Smoothening of microscopic variables, calculating gradients, time step solution to the modified NSE
+ * Pseudo Collision Operator, etc.
+ */
+class Simulator {
+
+    /**
+     * Constructor method for the simulator
+     * @param {number} N : number of particles in the simulation
+     * Note: This number can not be changed once simulation is created!
+     * Recommended value of N for fast perfomance : ####
+     * @param {number} h : Kernel distance for smoothening
+     */
+    constructor(N, h){
+        this.N = N;
+        this.particles = Array(N);
+        this.h = h;
+    }
+
+    /**
+     * 
+     * @param {number} qi     : coordinate value q of the ith particle
+     * @param {number} qj     : coordinate value q of the jth particle
+     * @param {number} choice : choice of kernel; 1 indicates gaussian, 2 indicates poly6
+     * and 3 indicates double cosine
+     */
+    kernel(qi, qj, choice){
+        let r = qi - qj;
+        let h = this.h;
+
+        switch(choice){
+
+            //GAUSSIAN KERNEL
+            case 1:
+                let normalg = 1;
+                return normalization * Math.exp(- Math.pow(r/h, 2));
+
+            //POLY6 KERNEL
+            case 2:
+                let normalp6 = 315/(64*Math.PI*Math.pow(h,9));
+                let distp6 = Math.pow(h,2) - Math.pow(r,2);
+
+                return normalp6 * Math.pow(distp6, 3);
+
+            //DOUBLE COSINE KERNEL
+            case 3:
+                let cos1 = Math.cos(Math.PI * r/h);
+                let cos2 = Math.cos(2*Math.PI * r/h);
+
+                return 4*cos1 + cos2 + 3;
+
+
+            default:
+                console.log("Select proper kernel!");
+        }
+
+    }
+
+}
+
 //THIS CODE GOES AT THE VERY BOTTOM
 
 let canvas =/**  @type {HTMLCanvasElement} */ (document.getElementById("canvas"));
